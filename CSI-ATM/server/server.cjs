a833bc3f2ade5dab614148e2bc54ea8c2fa41dc8
kwong <w00waykwong@gmail.com>
2024-07-03 21:59:17 +0800

    feat: 管理端访问响应

--- CSI-ATM/server/server.cjs
+++ CSI-ATM/server/server.cjs
@@ -28,20 +28,20 @@ app.use(bodyParser.json());
 
 //// 根据需求添加数据库请求 
 
-// app.post('/reserve', (req, res) => {
-//   const { canteen, floor, seat, time } = req.body;
+app.get('/getLog', (req, res) => {
+  const { canteen, floor, seat, time } = req.body;
   
-//   //3. 此处根据需求修改 SQL 语句
-//   const sql = 'INSERT INTO reservations (id, canteen, floor, seat, time) VALUES (NULL, ?, ?, ?, ?)';
-//   db.query(sql, [canteen, floor, seat, time], (err, result) => {
+  //3. 此处根据需求修改 SQL 语句
+  const sql = 'INSERT INTO reservations (id, canteen, floor, seat, time) VALUES (NULL, ?, ?, ?, ?)';
+  db.query(sql, [canteen, floor, seat, time], (err, result) => {
     
-//     if (err) {
-//       res.status(500).send({ error: 'Failed to save reservation' });
-//       return;
-//     }
-//     res.status(201).send({ message: 'Reservation saved successfully' });
-//   });
-// });
+    if (err) {
+      res.status(500).send({ error: 'Failed to save reservation' });
+      return;
+    }
+    res.status(201).send({ message: 'Reservation saved successfully' });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
