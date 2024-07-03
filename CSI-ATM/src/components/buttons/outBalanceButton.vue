e10af4d8f354466877098adb99d66d8eb95d5951
waywooKwong <2211992@mail.nankai.edu.cn>
2024-07-03 19:59:51 +0800

    fix: 修改组件显示问题, base on 架构 From WangPu

---
+++ CSI-ATM/src/components/buttons/outBalanceButton.vue
@@ -0,0 +1,10 @@
+<!-- 取款功能按钮，挂在HomeView.vue中 -->
+<template>
+  <div class="out">
+    <router-link to="/inBalanceView">
+      <button>取款</button>
+    </router-link>
+  </div>
+</template>
+<script></script>
+<style scoped></style>
---
+++ CSI-ATM/src/components/buttons/transBalanceButton.vue
@@ -0,0 +1,14 @@
+<!-- 转账功能按钮，挂在HomeView.vue中 -->
+<template>
+  <div class="trans">
+    <router-link to="/transBalanceView">
+      <button>转账</button>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  setup() {},
+};
+</script>
+<style scoped></style>
