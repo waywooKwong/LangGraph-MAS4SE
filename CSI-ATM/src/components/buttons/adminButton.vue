e9a3c7d5f1951e3971268324028ab3df8a0a9cd4
wenxiang <3211307497@qq.com>
2024-07-04 16:46:51 +0800

    style: Home主界面更新

--- CSI-ATM/src/components/buttons/adminButton.vue
+++ CSI-ATM/src/components/buttons/adminButton.vue
@@ -2,9 +2,22 @@
 <template>
   <div class="admin">
     <router-link to="/adminView">
-      <button>管理员登录</button>
+      <button class="button">管理员登录</button>
     </router-link>
   </div>
 </template>
 <script></script>
-<style scoped></style>
+<style scoped>
+.button{
+    background-color: #8f9192;
+      border: none;
+      border-radius: 4px;
+      color: white;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 12px;
+}
+
+</style>
