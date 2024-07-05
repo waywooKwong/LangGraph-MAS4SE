9d09192f3164e8942d055ebea473e45540707e8b
waywooKwong <2211992@mail.nankai.edu.cn>
2024-07-05 10:36:50 +0800

    feat: 转帐服务功能实现

--- CSI-ATM/server/server.cjs
+++ CSI-ATM/server/server.cjs
@@ -10,9 +10,9 @@ const port = 3000; // 这是固定的端口，最好不要修改
 // 2. 根据需求 create table
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'myuser',
-    password: 'wp200424',
-    database: 'test' //注意在 SCHEMAS 确认
+    user: 'canteen',
+    password: '',
+    database: 'dbcanteen' //注意在 SCHEMAS 确认
 });
 
 
@@ -77,6 +77,7 @@ app.put('/inBalance', (req, res) => {
     res.send({ success: 'inBalance updated successfully' });
   });
 });
+
 //存款服务更新日志
 app.post('/inBalanceInsertLog', (req, res) => {
   const newData = req.body;
@@ -92,6 +93,63 @@ app.post('/inBalanceInsertLog', (req, res) => {
     res.send({ success: 'inBalance updated successfully' });
   });
 });
+
+//// 转账服务的请求
+app.put('/transBalance', (req, res) => {
+  const { balance, account, targetAccount } = req.body;
+
+  // 第一个更新语句
+  const sql1 = 'UPDATE User SET balance = balance - ? WHERE account = ?';
+  const values1 = [balance, account];
+
+  db.query({ sql: sql1, values: values1, multipleStatements: false }, (err, result1) => {
+    if (err) {
+      res.status(500).send({ error: 'Failed to update User balance' });
+      return;
+    }
+
+    if (result1.affectedRows === 0) {
+      res.status(404).send({ error: 'Source User not found' });
+      return;
+    }
+
+    // 第二个更新语句
+    const sql2 = 'UPDATE User SET balance = balance + ? WHERE account = ?';
+    const values2 = [balance, targetAccount];
+
+    db.query({ sql: sql2, values: values2, multipleStatements: false }, (err, result2) => {
+      if (err) {
+        res.status(500).send({ error: 'Failed to update User balance' });
+        return;
+      }
+
+      if (result2.affectedRows === 0) {
+        res.status(404).send({ error: 'Target User not found' });
+        return;
+      }
+
+      res.send({ success: 'Balance updated successfully' });
+    });
+  });
+});
+
+
+//// 转账服务更新日志
+app.post('/transBalanceInsertLog', (req, res) => {
+  const newData = req.body;
+  const sql = 'INSERT INTO Log (timestamp, event, object, balance, state, User_user_id) VALUES (?,?,?,?,?,?)';
+  const values = [newData.timestamp, newData.event, newData.object, newData.balance, newData.state, newData.User_user_id];
+
+  db.query(sql, values, (err, results) => {
+    if (err) {
+      console.error('日志插入失败: ', err); // 打印错误信息
+      res.status(500).send({ error: 'Failed to update Log' });
+      return;
+    }
+    res.send({ success: 'transBalance Log insert successfully' });
+  });
+});
+
 
 // GET 请求处理，根据账号 ID 查询用户数据
 app.get('/UserData', (req, res) => {
