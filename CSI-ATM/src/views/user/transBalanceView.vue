d068d7af82ccd1a823eb1d5a979eb886dead2b5f
kwong <w00waykwong@gmail.com>
2024-07-05 19:08:34 +0800

    perf: Prettier统一代码风格

--- CSI-ATM/src/views/user/transBalanceView.vue
+++ CSI-ATM/src/views/user/transBalanceView.vue
@@ -1,19 +1,15 @@
- <!-- 转账页面 -->
+<!-- 转账页面 -->
 <template>
   <div class="container">
-    <div class="outBanlance-title">
-       转账界面 
-    </div>
+    <div class="outBanlance-title">转账界面</div>
     <main>
       <TransBalance />
     </main>
   </div>
-  
-  
 </template>
 <script>
-import transBalance from "@/components/transBalance.vue";
-import { onMounted } from 'vue'; 
+import transBalance from '@/components/transBalance.vue'
+import { onMounted } from 'vue'
 
 export default {
   components: {
@@ -22,16 +18,18 @@ export default {
   setup() {
     // 设置图标
     onMounted(() => {
-      setFavicon('/icons/favicon-balance.ico');
-    });
+      setFavicon('/icons/favicon-balance.ico')
+    })
   },
-};
+}
 function setFavicon(href) {
-  let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-  link.type = 'image/x-icon';
-  link.rel = 'shortcut icon';
-  link.href = href;
-  document.getElementsByTagName('head')[0].appendChild(link);
+  let link =
+    document.querySelector("link[rel*='icon']") ||
+    document.createElement('link')
+  link.type = 'image/x-icon'
+  link.rel = 'shortcut icon'
+  link.href = href
+  document.getElementsByTagName('head')[0].appendChild(link)
 }
 </script>
 <style scoped>
@@ -49,13 +47,10 @@ function setFavicon(href) {
   justify-content: center;
   align-items: center;
 }
-.outBanlance-title{
+.outBanlance-title {
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
- font-size: 200%;
+  font-size: 200%;
   margin-right: 200px;
   margin-bottom: 50px;
 }
-
-
-
 </style>
