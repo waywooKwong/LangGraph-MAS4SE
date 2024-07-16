22fd1388c0622534e8bfb74c7ad0bcba2c007433
kwong <w00waykwong@gmail.com>
2024-07-16 19:05:57 +0800

    initial: new project MAS:Multi-Agents System

---
+++ MAS/src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>智能对话系统</h1>
+    <input v-model="query" placeholder="输入你的问题" />
+    <button @click="sendQuery">发送</button>
+    <p v-if="response">回复: {{ response }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+const apiClinet = axios.create({
+  baseURL: 'http://10.22.14.142:9090',
+  headers:{
+    'Content-Type':'application/json'
+  }
+});
+
+export default {
+  data() {
+    return {
+      query: "",
+      response: "",
+    };
+  },
+  methods: {
+    async sendQuery() {
+      try {
+        const res = await apiClinet.post("/query", {
+          query: this.query,
+        });
+        this.response = res.data.response;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.hello {
+  text-align: center;
+  margin-top: 50px;
+}
+input {
+  padding: 10px;
+  margin-right: 10px;
+}
+button {
+  padding: 10px;
+}
+</style>
