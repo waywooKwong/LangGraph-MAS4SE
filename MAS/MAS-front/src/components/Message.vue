997c7749f3dabbe40bb2dd9ed1e02a22a0be4163
qian-cheng-01 <qianc_1@163.com>
2024-07-23 17:25:59 +0800

    feat：彻底更新web界面

---
+++ MAS/MAS-front/src/components/Message.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="['message', sender]">
+    <img :src="getAvatar(sender)" class="avatar" />
+    <span class="message-text">{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatMessage',
+  props: {
+    text: String,
+    sender: String,
+  },
+  methods: {
+    getAvatar(sender) {
+      if (sender === 'user') {
+        return '/icons/用户.png';
+      } else {
+        return '/icons/Bot.png';
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.message {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.message.user {
+  flex-direction: row-reverse;
+  background-color: #409eff;
+  color: white;
+  text-align: right;
+}
+
+.message.bot {
+  flex-direction: row;
+  background-color: #f1f1f1;
+  color: black;
+  text-align: left;
+}
+
+.message .avatar {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin: 0 10px;
+}
+
+.message-text {
+  max-width: 70%;
+  word-wrap: break-word;
+}
+</style>
--- MAS/MAS-front/vite.config.js
+++ MAS/MAS-front/vite.config.js
@@ -5,15 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 8080, // You can change the port as needed
-    allowedHosts: 'all' // Allow all hosts
   }
 })
