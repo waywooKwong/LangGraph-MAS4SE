4fcbae4fde53d6c34fb235cdb37699cf95f03e8f
wenxiang <3211307497@qq.com>
2024-07-23 18:59:25 +0800

    merge: 更新状态

---
+++ MAS/MAS-front/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <el-container style="height: 100vh;">
+      <Sidebar />
+      <el-container>
+        <el-header class="header">ChatGPT UI</el-header>
+        <el-main class="main">
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/MainSidebar.vue';
+
+export default {
+  components: {
+    Sidebar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.el-container {
+  height: 100vh;
+}
+
+.header {
+  background-color: #409EFF;
+  color: white;
+  text-align: center;
+  padding: 15px;
+}
+
+.main {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.sidebar {
+  background-color: #f4f4f4;
+  border-right: 1px solid #dcdfe6;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.el-menu {
+  border-right: none;
+}
+
+.el-menu-item {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+}
+
+.el-menu-item i {
+  margin-right: 10px;
+}
+</style>
