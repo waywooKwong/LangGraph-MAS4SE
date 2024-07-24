275a93489784638af7edb94f24a05ea51d6a9899
wenxiang <3211307497@qq.com>
2024-07-24 18:02:19 +0800

    docx: 优化代码框架

--- MAS/MAS-front/src/App.vue
+++ MAS/MAS-front/src/App.vue
@@ -3,9 +3,14 @@
     <el-container style="height: 100vh;">
       <Sidebar />
       <el-container>
-        <el-header class="header">ChatGPT UI</el-header>
+        <el-header class="header">主页面标题</el-header>
         <el-main class="main">
+
+
           <router-view />
+
+
+          
         </el-main>
       </el-container>
     </el-container>
@@ -17,7 +22,7 @@ import Sidebar from '@/components/MainSidebar.vue';
 
 export default {
   components: {
-    Sidebar,
+     Sidebar,
   },
 };
 </script>
@@ -31,9 +36,7 @@ export default {
   color: #2c3e50;
 }
 
-.el-container {
-  height: 100vh;
-}
+
 
 .header {
   background-color: #409EFF;
@@ -47,27 +50,5 @@ export default {
   display: flex;
   flex-direction: column;
   overflow: hidden;
-}
-
-.sidebar {
-  background-color: #f4f4f4;
-  border-right: 1px solid #dcdfe6;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-}
-
-.el-menu {
-  border-right: none;
-}
-
-.el-menu-item {
-  display: flex;
-  align-items: center;
-  padding: 10px;
-}
-
-.el-menu-item i {
-  margin-right: 10px;
 }
 </style>
