76b7a83747472848b685c5cbfa5313c0c0840226
wenxiang <3211307497@qq.com>
2024-08-01 16:37:38 +0800

    style: 更改历史记录显示效果

--- MAS/MAS-front 3.0/src/axios.ts
+++ MAS/MAS-front 3.0/src/axios.ts
@@ -2,12 +2,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-<<<<<<< HEAD
   // baseURL: 'http://10.22.63.73:8000', // 后端服务地址
   baseURL: 'http://10.22.3.87:8000', // 后端服务地址
-=======
-  baseURL: 'http://10.22.64.78:8000', // 后端服务地址
->>>>>>> e35378e538dbd2c386e735f26065b3b09618cddc
   headers: {
     'Content-Type': 'application/json',
   },
