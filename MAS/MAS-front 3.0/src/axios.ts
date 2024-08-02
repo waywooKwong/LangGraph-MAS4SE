6517f3e23b0d959124d474db0cb96c7d941b482c
wp2022nk <517717562@qq.com>
2024-08-02 16:23:21 +0800

    feat: 添加点击满意后的弹窗，目前缺少一个跳转

--- MAS/MAS-front 3.0/src/axios.ts
+++ MAS/MAS-front 3.0/src/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://10.22.1.244:8000', // 后端服务地址
+  baseURL: 'http://10.22.3.87:8000', // 后端服务地址
   headers: {
     'Content-Type': 'application/json',
   },
