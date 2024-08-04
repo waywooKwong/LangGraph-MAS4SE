d469d9dcd2375c48fbcd577fdf9777447d1eb734
qian-cheng-01 <qianc_1@163.com>
2024-08-04 11:45:39 +0800

    feat: 修改侧边栏

--- MAS/MAS-front 3.0/src/router.ts
+++ MAS/MAS-front 3.0/src/router.ts
@@ -4,6 +4,7 @@ import ChatView from '@/views/ChatView.vue';
 import AgentMap from '@/views/AgentMap.vue';
 import StartView from '@/views/StartView.vue';
 import LoadPage from '@/views/LoadPage.vue';
+import MainSidebar from './components/MainSidebar.vue';
 
 Vue.use(VueRouter);
 
@@ -27,6 +28,11 @@ const routes = [
     path: '/load',
     name: 'LoadPage',
     component: LoadPage,
+  },
+  {
+    path: '/s',
+    name: 'MainSidebar',
+    component: MainSidebar,
   },
 ];
 
