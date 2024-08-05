b81a4d9acbf74ed3001cd46c24d7d19131b839ba
qian-cheng-01 <qianc_1@163.com>
2024-08-05 11:30:44 +0800

    merge: 处理冲突

--- MAS/MAS-front 3.0/src/axios.ts
+++ MAS/MAS-front 3.0/src/axios.ts
@@ -2,12 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-<<<<<<< HEAD
   baseURL: 'http://10.22.188.216:8000', // 后端服务地址
-=======
-  // baseURL: 'http://10.22.63.73:8000', // 后端服务地址
-  baseURL: 'http://10.22.3.87:8000', // 后端服务地址
->>>>>>> 030ff2c91c186cce9e9018e47bd7e8a786f97ae3
   headers: {
     'Content-Type': 'application/json',
   },
