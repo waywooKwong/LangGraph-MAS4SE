12b606d253d785e635e9f00f08421864abbac144
qian-cheng-01 <qianc_1@163.com>
2024-08-12 10:52:46 +0800

    fix: 前端缺少文件问题

--- MAS4SE-frontend/src/axios.ts
+++ MAS4SE-frontend/src/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://10.22.179.7:8000', // 后端服务地址
+  baseURL: 'http://192.168.194.233:8000', // 后端服务地址
   headers: {
     'Content-Type': 'application/json',
   },
