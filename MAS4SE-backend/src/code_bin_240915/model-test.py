e2a4720fe94c7c6f44e3690c3de76af900500e84
waywooKwong <2211992@mail.nankai.edu.cn>
2025-03-15 00:52:40 +0800

    fix: clean bin code & prepare for new generation

--- MAS4SE-backend/src/code_bin_240915/model-test.py
+++
@@ -1,6 +0,0 @@
-from ModelChoise import modelchoise
-# chat_model = modelchoise.get_spark_chat_model()
-from langchain_community.chat_models import ChatOllama
-chat_model = ChatOllama(model = "qwen2")
-response = chat_model.invoke("introduce yourself")
-print("response:",response)
